98917f2b79786c38c0948ff119fd3b72fb1fb99b
acreel21 <acreel@gmu.edu>
2019-03-26 20:53:56 -0400

    Final Updates

--- WIFI_AP.ino
+++ WIFI_AP.ino
@@ -6,6 +6,8 @@ char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;                // your network key Index number (needed only for WEP)
 IPAddress ip;
+int status = WL_IDLE_STATUS;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,15 +16,44 @@ void setup() {
   
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
-
-  WiFi.beginAP(ssid); 
-
-  ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+  
+  status = WiFi.beginAP(ssid, pass, 1); 
+  if (status != WL_AP_LISTENING) {
+    Serial.println("Creating access point failed");
+    // don't continue
+    while (true);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    if (status != WiFi.status()) {
+    // it has changed update the variable
+    status = WiFi.status();
 
+    if (status == WL_AP_CONNECTED) {
+      byte remoteMac[6];
+
+      // a device has connected to the AP
+      Serial.print("Device connected to AP, MAC address: ");
+      WiFi.APClientMacAddress(remoteMac);
+      printMacAddress(remoteMac);
+    } else {
+      // a device has disconnected from the AP, and we are back in listening mode
+      Serial.println("Device disconnected from AP");
+    }
+  }
+}
+
+void printMacAddress(byte mac[]) {
+  for (int i = 5; i >= 0; i--) {
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0) {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
 }
